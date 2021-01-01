50c48d0cb2d60a8f6116c678e2fed9c116ad944e
gizempesen <gizempesen999@gmail.com>
2021-01-02 02:59:30 +0300

    splash screen text sizes and colors

--- RBook/app/src/main/java/com/example/r_book/SplashScreenActivity.java
+++ RBook/app/src/main/java/com/example/r_book/SplashScreenActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.View;
 
 import gr.net.maroulis.library.EasySplashScreen;
@@ -19,8 +20,8 @@ public class SplashScreenActivity extends AppCompatActivity {
                 .withTargetActivity(LoginActivity.class)
                 .withSplashTimeOut(3000)
                 .withBackgroundColor(Color.parseColor("#1a1b29"))
-                .withHeaderText("Header")
-                .withFooterText("Footer")
+                .withHeaderText("  ")
+                .withFooterText("  ")
                 .withBeforeLogoText(" WELCOME ")
                 .withAfterLogoText("R-BOOK")
                 .withLogo(R.drawable.icon);
@@ -29,6 +30,8 @@ public class SplashScreenActivity extends AppCompatActivity {
         config.getFooterTextView().setTextColor(Color.WHITE);
         config.getBeforeLogoTextView().setTextColor(Color.WHITE);
         config.getAfterLogoTextView().setTextColor(Color.WHITE);
+        config.getAfterLogoTextView().setTextSize(TypedValue.COMPLEX_UNIT_SP, 40);
+        config.getBeforeLogoTextView().setTextSize(TypedValue.COMPLEX_UNIT_SP, 40);
 
         View easySplashScreen = config.create();
         setContentView(easySplashScreen);
