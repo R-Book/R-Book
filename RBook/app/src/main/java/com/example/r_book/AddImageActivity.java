0f38ea414be4ea83e127403d68b6caee9c3d223a
gizempesen <gizempesen999@gmail.com>
2021-01-03 18:39:32 +0300

    Adding image xml file is designed.

---
+++ RBook/app/src/main/java/com/example/r_book/AddImageActivity.java
@@ -0,0 +1,14 @@
+package com.example.r_book;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_image);
+    }
+}
