08437240a1ddcafef48aefab77bb25fe647486c0
gizempesen <gizempesen999@gmail.com>
2021-01-04 00:52:26 +0300

    Add image screen Homescreen and connecting camera

--- RBook/app/src/main/java/com/example/r_book/HomeScreenActivity.java
+++ RBook/app/src/main/java/com/example/r_book/HomeScreenActivity.java
@@ -1,14 +1,57 @@
 package com.example.r_book;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeScreenActivity extends AppCompatActivity {
+
+    List<Post> posts = new ArrayList<>();
+    ListView listView;
+    static final int POST_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+        Log.d("Dev", "Burdayım");
+
+        listView = findViewById(R.id.listView);
+        Log.d("Dev", "Burdayım2");
+        listView.setAdapter(new PostAdapter(this, posts));
+
+        Button btnPost = findViewById(R.id.btnPost);
+        btnPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeScreenActivity.this, PostActivity.class);
+                startActivityForResult(intent, POST_REQUEST);
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == POST_REQUEST && resultCode == Activity.RESULT_OK){
+            Post post = new Post();
+            post.setMessage(data.getCharSequenceExtra("msg").toString());
+            post.setImage((Bitmap) data.getParcelableExtra("bitmap"));
+            posts.add(post);
+            ((PostAdapter) listView.getAdapter()).notifyDataSetChanged();
+        }
     }
 }
