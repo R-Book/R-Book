4d9e2184c890033c4988b23cfcedc94f336d0361
fatmakaradag <64332825+fatmakaradag@users.noreply.github.com>
2021-01-06 02:39:38 +0300

    Update RegisterActivity.java

--- RBook/app/src/main/java/com/example/r_book/RegisterActivity.java
+++ RBook/app/src/main/java/com/example/r_book/RegisterActivity.java
@@ -9,57 +9,62 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class RegisterActivity extends AppCompatActivity {
-
+public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
-    EditText emailText, passwordText;
+    EditText usernameText,emailtxt, passwordText,passwordtxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_register);
+        setContentView(R.layout.activity_login);
 
         firebaseAuth = FirebaseAuth.getInstance();    /*değer atadık*/
-        emailText = findViewById(R.id.email);
+        usernameText = findViewById(R.id.usernameText);
+        emailtxt = findViewById(R.id.emailtxt);
         passwordText = findViewById(R.id.txtpassword);
+        passwordtxt = findViewById(R.id.passwordTxt);
 
-        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        FirebaseUser firebaseUser=firebaseAuth.getCurrentUser();
 
-        if (firebaseUser != null) {
-            Intent intent = new Intent(RegisterActivity.this, HomeScreenActivity.class); /*başarılı olunca gideceği yer*/
+        if(firebaseUser != null){
+            Intent intent=new Intent(RegesterActivity.this,HomeActivity.class); /*başarılı olunca gideceği yer*/
             startActivity(intent);
         }
 
     }
 
-    public void loginClicked(View view) {
-        String email = emailText.getText().toString();
-        String password = passwordText.getText().toString();
+    public void backClicked(View view) {
+        String username=usernameText.getText().toString();
+        String email=emailtxt.getText().toString();
+        String password=passwordText.getText().toString();
+        String confirmpassword=passwordtxt.getText().toString();
 
-        firebaseAuth.signInWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+        Intent intent=new Intent(RegesterActivity.this,HomeActivity.class); /*kullanıcı istemezse direk home ekranına geçiş yapar*/
+        startActivity(intent);
+
+
+    }
+    public void loginClicked(View view) {
+        String username=usernameText.getText().toString();
+        String email=emailtxt.getText().toString();
+        String password=passwordText.getText().toString();
+        String confirmpassword=passwordtxt.getText().toString();
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
             @Override
             public void onSuccess(AuthResult authResult) {
-                Toast.makeText(RegisterActivity.this, "User Registed", Toast.LENGTH_LONG).show();
-                Intent intent = new Intent(RegesterActivity.this, HomeActivity.class); /*başarılı olunca gideceği yer*/
+                Toast.makeText(LoginActivity.this,"User Regested",Toast.LENGTH_LONG).show();
+                Intent intent=new Intent(RegesterActivity.this,HomeActivity.class); /*başarılı olunca gideceği yer,regester olunca homescreene gider*/
                 startActivity(intent);
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
-                Toast.makeText(RegisterActivity.this, e.getLocalizedMessage().toString(), Toast.LENGTH_LONG).show(); /*message*/
+                Toast.makeText(RegesterActivity.this,e.getLocalizedMessage().toString(),Toast.LENGTH_LONG).show(); /*message*/
 
             }
 
         });
-
+    }
     }
 
-    public void registerClicked(View view) {
-        String email = emailText.getText().toString();
-        String password = passwordText.getText().toString();
-
-        Intent intent=new Intent(RegisterActivity.this,LoginActivity.class); /*regester basınca login ekranına gidecek*/
-        startActivity(intent);
-
-    }
-}
