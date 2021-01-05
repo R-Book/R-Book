ac80a5c3ee2976de849f3f5118560e045d420be0
Gizem Pesen <pesengizem999@gmail.com>
2021-01-06 02:33:39 +0300

    Update RegisterActivity.java

--- RBook/app/src/main/java/com/example/r_book/RegisterActivity.java
+++ RBook/app/src/main/java/com/example/r_book/RegisterActivity.java
@@ -9,7 +9,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class RegesterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;
     EditText emailText, passwordText;
@@ -17,16 +17,16 @@ public class RegesterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_regester);
+        setContentView(R.layout.activity_login_register);
 
         firebaseAuth = FirebaseAuth.getInstance();    /*değer atadık*/
-        emailText = findViewById(R.id.usernameText);
+        emailText = findViewById(R.id.email);
         passwordText = findViewById(R.id.txtpassword);
 
         FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
 
         if (firebaseUser != null) {
-            Intent intent = new Intent(RegesterActivity.this, HomeActivity.class); /*başarılı olunca gideceği yer*/
+            Intent intent = new Intent(RegisterActivity.this, HomeScreenActivity.class); /*başarılı olunca gideceği yer*/
             startActivity(intent);
         }
 
@@ -39,14 +39,14 @@ public class RegesterActivity extends AppCompatActivity {
         firebaseAuth.signInWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
             @Override
             public void onSuccess(AuthResult authResult) {
-                Toast.makeText(RegesterActivity.this, "User Regested", Toast.LENGTH_LONG).show();
+                Toast.makeText(RegisterActivity.this, "User Registed", Toast.LENGTH_LONG).show();
                 Intent intent = new Intent(RegesterActivity.this, HomeActivity.class); /*başarılı olunca gideceği yer*/
                 startActivity(intent);
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
-                Toast.makeText(RegesterActivity.this, e.getLocalizedMessage().toString(), Toast.LENGTH_LONG).show(); /*message*/
+                Toast.makeText(RegisterActivity.this, e.getLocalizedMessage().toString(), Toast.LENGTH_LONG).show(); /*message*/
 
             }
 
@@ -54,11 +54,11 @@ public class RegesterActivity extends AppCompatActivity {
 
     }
 
-    public void regesterClicked(View view) {
+    public void registerClicked(View view) {
         String email = emailText.getText().toString();
         String password = passwordText.getText().toString();
 
-        Intent intent=new Intent(RegesterActivity.this,LoginActivity.class); /*regester basınca login ekranına gidecek*/
+        Intent intent=new Intent(RegisterActivity.this,LoginActivity.class); /*regester basınca login ekranına gidecek*/
         startActivity(intent);
 
     }
