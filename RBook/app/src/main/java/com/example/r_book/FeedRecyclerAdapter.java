e6a868d60d86948ae1b3519ffc00fadf27e50fef
gizempesen <gizempesen999@gmail.com>
2021-01-11 06:18:06 +0300

    The books informations are published on home page successfully .

---
+++ RBook/app/src/main/java/com/example/r_book/FeedRecyclerAdapter.java
@@ -0,0 +1,78 @@
+package com.example.r_book;
+
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.squareup.picasso.Picasso;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import java.util.ArrayList;
+
+public class FeedRecyclerAdapter extends RecyclerView.Adapter<FeedRecyclerAdapter.PostHolder> {
+
+    private ArrayList<String> userEmailList;
+    private ArrayList<String> NameList;
+    private ArrayList<String> PriceList;
+    private ArrayList<String> OtherList;
+    private ArrayList<String> userImageList;
+
+
+    public FeedRecyclerAdapter(ArrayList<String> userEmailList,ArrayList<String> OtherList,ArrayList<String> PriceList, ArrayList<String> NameList, ArrayList<String> userImageList) {
+        this.userEmailList = userEmailList;
+        this.NameList = NameList;
+        this.PriceList = PriceList;
+        this.OtherList = OtherList;
+        this.userImageList = userImageList;
+    }
+
+    @NonNull
+    @Override
+    public PostHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.row,parent,false);
+        return new PostHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PostHolder holder, int position) {
+
+        holder.userEmailText.setText(userEmailList.get(position));
+        holder.NameText.setText(NameList.get(position));
+        holder.PriceText.setText(PriceList.get(position));
+        holder.OtherText.setText(OtherList.get(position));
+        Picasso.get().load(userImageList.get(position)).into(holder.imageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return userEmailList.size();
+    }
+
+    class PostHolder extends RecyclerView.ViewHolder {
+
+        ImageView imageView;
+        TextView userEmailText;
+        TextView NameText;
+        TextView PriceText;
+        TextView OtherText;
+
+        public PostHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imageView = itemView.findViewById(R.id.recyclerview_row_imageview);
+            userEmailText = itemView.findViewById(R.id.recyclerview_row_useremail_text);
+            NameText = itemView.findViewById(R.id.recyclerview_row_name_text);
+            PriceText = itemView.findViewById(R.id.recyclerview_row_price_text);
+            OtherText = itemView.findViewById(R.id.recyclerview_row_other_text);
+
+
+        }
+    }
+}
--- RBook/app/src/main/java/com/example/r_book/HomeScreenActivity.java
+++ RBook/app/src/main/java/com/example/r_book/HomeScreenActivity.java
@@ -3,8 +3,11 @@ package com.example.r_book;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Activity;
+import android.app.DownloadManager;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
@@ -14,18 +17,35 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class HomeScreenActivity extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
 
     List<Post> posts = new ArrayList<>();
-    ListView listView;
+    RecyclerView recyclerView;
     static final int POST_REQUEST = 1;
+
+    private FirebaseFirestore firebaseFirestore;
+    ArrayList<String> userEmailFromFB;
+    ArrayList<String> userPriceFromFB;
+    ArrayList<String> userNameFromFB;
+    ArrayList<String> userOtherFromFB;
+    ArrayList<String> userImageFromFB;
+    FeedRecyclerAdapter feedRecyclerAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +53,21 @@ public class HomeScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
-        listView = findViewById(R.id.listView);   //create listview
-        listView.setAdapter(new PostAdapter(this, posts));   //adapter object
+        userNameFromFB = new ArrayList<>();
+        userPriceFromFB = new ArrayList<>();
+        userOtherFromFB = new ArrayList<>();
+        userEmailFromFB = new ArrayList<>();
+        userImageFromFB = new ArrayList<>();/*burdan tam emin değilim şimdilik dursun*/
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
+        getDataFromFirestore();
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        feedRecyclerAdapter = new FeedRecyclerAdapter(userEmailFromFB,userNameFromFB,userPriceFromFB,userOtherFromFB,userImageFromFB);
+        recyclerView.setAdapter(feedRecyclerAdapter);
 
         Button btnPost = findViewById(R.id.btnPost);   /*post button on click*/
         btnPost.setOnClickListener(new View.OnClickListener() {
@@ -57,7 +90,7 @@ public class HomeScreenActivity extends AppCompatActivity {
             post.setOther(data.getCharSequenceExtra("other").toString());
             post.setImage((Bitmap) data.getParcelableExtra("bitmap"));
             posts.add(post);
-            ((PostAdapter) listView.getAdapter()).notifyDataSetChanged();
+            ((FeedRecyclerAdapter) recyclerView.getAdapter()).notifyDataSetChanged();
         }
     }
 
@@ -82,4 +115,45 @@ public class HomeScreenActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void getDataFromFirestore(){
+
+        CollectionReference collectionReference = firebaseFirestore.collection("Posts");
+
+        collectionReference.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+
+                if (e != null) {
+                    Toast.makeText(HomeScreenActivity.this,e.getLocalizedMessage().toString(),Toast.LENGTH_LONG).show();
+                }
+
+                if (queryDocumentSnapshots != null) {
+
+                    for (DocumentSnapshot snapshot : queryDocumentSnapshots.getDocuments()) {
+
+                        Map<String,Object> data = snapshot.getData();
+
+                        //Casting
+
+                        String name = (String) data.get("bookname");
+                        String price = (String) data.get("bookprice");
+                        String other = (String) data.get("bookotherinfo");
+                        String userEmail = (String) data.get("useremail");
+                        String downloadUrl = (String) data.get("downloadurl");
+
+                        userNameFromFB.add(name);
+                        userPriceFromFB.add(price);
+                        userOtherFromFB.add(other);
+                        userEmailFromFB.add(userEmail);
+                        userImageFromFB.add(downloadUrl);
+
+                        feedRecyclerAdapter.notifyDataSetChanged();
+                    }
+                }
+            }
+        });
+    }
 }
+
+
+
